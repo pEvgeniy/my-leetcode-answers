d5c866d290a720e616ef8f8f9ed93f4da4faedb6
pEvgeniy <PopEvgeniyV@yandex.ru>
2023-02-22 17:42:02 +0300

    new answers

--- src/main/java/Solutions.java
+++ src/main/java/Solutions.java
@@ -370,12 +370,64 @@ public class Solutions {
     }
 
     public static List<List<Integer>> threeSum(int[] nums) {
-        List<Set<Integer>> setList = new ArrayList<>();
-        Set<Integer> uniqueTriplet = new HashSet<>();
-
-        for (int i = 0; i < nums.length; i++) {
-
+        List<List<Integer>> list = new ArrayList<>();
+        if (nums.length < 3) {
+            return Collections.emptyList();
         }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length-2; i++) {
+            if (nums[i] > 0) break;
+            int j = i + 1;
+            int k = nums.length - 1;
 
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    list.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    while (j < k && nums[j] == nums[j + 1]) {
+                        j++;
+                    }
+                    while (j < k && nums[k] == nums[k - 1]) {
+                        k--;
+                    }
+                    j++;
+                    k--;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    j++;
+                }
+                while (i < nums.length-2 && nums[i] == nums[i + 1]) {
+                    i++;
+                }
+            }
+        }
+        return list;
+    }
+
+    public static int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int closestSum = nums[0] + nums[1] + nums[nums.length - 1];
+        boolean firstIter = true;
+        for (int i = 0; i < nums.length - 2; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == target) {
+                    return sum;
+                }
+                if (Math.abs(target - sum) < (Math.abs(target - closestSum))) {
+                    closestSum = sum;
+                }
+                if (sum > target) {
+                    k--;
+                }
+                if (sum < target) {
+                    j++;
+                }
+            }
+        }
+        return closestSum;
     }
 }
